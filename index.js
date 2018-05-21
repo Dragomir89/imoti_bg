f50bae70a71a276274b3571870da10e1ce411321
Dragomir Mildov <drashoo@abv.bg>
2018-05-21 13:34:30 +0300

    add gitignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send({test:'test'})
-})
-
+passport.use(new GoogleStrategy())
+// clientID 225854675802-jhoqja2ij7kih6iqluj034ns7rbgi9jo.apps.googleusercontent.com
+// clientSecret Jjpl2uz6aRldZJgq5NTERmux
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
