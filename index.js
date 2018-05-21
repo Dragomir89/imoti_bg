8d10863a89696e814180dc7c7016ee7834c2f025
Dragomir Mildov <drashoo@abv.bg>
2018-05-21 13:43:58 +0300

    add google keys

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-
+const keys = require('./config/keys')
 const app = express()
 
-passport.use(new GoogleStrategy())
-// clientID 225854675802-jhoqja2ij7kih6iqluj034ns7rbgi9jo.apps.googleusercontent.com
-// clientSecret Jjpl2uz6aRldZJgq5NTERmux
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID, 
+    clientSecret: keys.googleClientSecret
+}))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>{
