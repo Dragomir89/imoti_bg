5e93e021ae32fee9a0a61e05fd6b5496e53c5023
Dragomir Mildov <drashoo@abv.bg>
2018-08-23 16:37:55 +0300

    add: reducers index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,12 @@ import authReducer from './authReducer'
 import myReducer from './myReducer'
 import addOptionsReducer from './addOptionsReducer'
 import showOffersReducer from './showOffersReducer'
-
+import getOptionsReducer from './getOptionsReducer'
 
 export default combineReducers({
     auth: authReducer,
     myReducer,
     addOptionsReducer,
-    showOffersReducer
+    showOffersReducer,
+    getOptionsReducer
 })
