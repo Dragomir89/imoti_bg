39816e7ba9db844e036c56210a2aec26300828f0
Dragomir Mildov <drashoo@abv.bg>
2018-09-10 09:06:55 +0300

    Refactor: Get Offers logic is moved to offers controller

--- routes/offers.js
+++ routes/offers.js
@@ -13,27 +13,9 @@ const url = require('url')
 
 
 module.exports = (app) =>{
-    app.get('/api/offers', (req, res)=>{
-        
-
-        offersCtrl.getAllOffers().then((offers)=>{
-            console.log(offers)
-            res.send(offers)
-        })
-    })
-
-    app.post('/api/construction-type',(req, res)=>{
-        res.send(req.body)
-    })
-
-    app.get('/api/test',(req, res)=>{
-        res.send({'test': 'test'})
-    })
-    ///////////////////////////////////////////////////////////////////////////
-
-
-
-    app.post('/api/add-details', (req,res)=>{
+    
+ 
+    app.post('/api/add-details', (req, res)=>{
         
         let data = req.body
         let promiseArr = []
@@ -61,7 +43,7 @@ module.exports = (app) =>{
         })
     })
 
-    app.get('/api/options', (req,res)=>{
+    app.get('/api/options', (req, res)=>{
 
         let returnObj = {}
 
@@ -82,10 +64,73 @@ module.exports = (app) =>{
 
     })
 
+    app.get('/api/get-offers/:page', (req, res)=> {
+        
+        const urlParts = url.parse(req.url, true)
+        const queryParams = urlParts.query
+        const offersPerPage = 10
+        const page =  Number(req.params.page)
+        const { phoneNumber } = queryParams
+
+        console.log(queryParams)
+
+
+        phoneNumber ?        
+        offersCtrl.findOffersByPhone(phoneNumber, page, offersPerPage)
+        .then((resObj)=>{ res.send(resObj) })
+        :
+        offersCtrl.getAllOffers(queryParams, page, offersPerPage)
+        .then((resObj)=>{ res.send(resObj) })        
+    })
+
+    app.get('/api/offer/:id', (req, res)=>{
+        offersCtrl.getOffer(req.params.id).then((offer)=>{ res.send(offer) })
+    })
+
+    app.put('/api/offer/:id', (req, res) => {
+        console.log('/api/offer (UPDATE): =============')
+        console.log(req.body)
+        // console.log(req.params)
+        const newOffer = {
+            area:         req.body.area,
+            description:  req.body.description,
+            phoneNumber:  req.body.phoneNumber,
+            // phoneNumbers: [{type: String, type: String, lowercase: true, trim: true}],
+            price:       Number(req.body.price),
+            address:     req.body.address,
+            info:        req.body.info,
+            propertyOwnerName: req.body.propertyOwnerName,
+            floor:       Number(req.body.floor),
+            constructionTypeId: req.body.constructionType,
+            propertyTypeId:     req.body.propertyType,
+            state:              req.body.state,
+            neighborhoodId:     req.body.neighborhood,
+            lastCall:           new Date(req.body.lastCall),
+            nextCall:           new Date(req.body.nextCall)   
+        }
+
+        Offer.update(
+            {_id:req.params.id}, 
+            newOffer, 
+            {multi: false}, 
+            function(err, newObj){
+                if(err){
+                    console.log('ERROR')
+                    console.log(err)
+                    res.send(err)
+                    return
+                }
+
+                console.log('updated obj')
+                console.log(newObj)
+
+                res.send(newObj)
+        })
+    })
+
     app.post('/api/post-offer',(req, res)=>{
         console.log('/api/post-offer')
 
-        
         console.log(req.body)
         let sendData = req.body
         let userId = req.user ? req.user._id : null
@@ -160,194 +205,5 @@ module.exports = (app) =>{
                 console.log(err)
                 res.send({error: err})
             })
-    })
-
-    app.get('/api/add-details', (req,res)=>{
-        console.log(req.body)
-        res.send(req.body)
-        return
-        offersCtrl.addNewDetails(req.body).save((data)=>{
-            console.log('SUCCESS REQUEST')
-            res.send({element:'data'})
-        })
-    })
-
-    app.get('/api/get-offers/:page', (req,res)=> {
-
-        const urlParts = url.parse(req.url, true)
-        const queryParams = urlParts.query
-       // console.log('queryParams -->')
-        //console.log(queryParams)
-        let searchObj = null
-
-        if(queryParams) {
-            searchObj = {}
-            if(queryParams.constructionType) { 
-                searchObj.constructionTypeId = queryParams.constructionType 
-            }
-            if(queryParams.propertyType){
-                searchObj.propertyTypeId = queryParams.propertyType 
-            }
-            if(queryParams.state){
-                searchObj.state = queryParams.state 
-            }
-            if(queryParams.neighborhood){
-                searchObj.neighborhoodId = queryParams.neighborhood 
-            }
-            if(queryParams.nextCall && queryParams.nextCall !== 'Invalid date'){
-                searchObj.nextCall = {"$gte": new Date(queryParams.nextCall)}
-            }
-            
-        }
-        
-
-        let page =  Number(req.params.page)
-        let offersPerPage = 10
-
-        let skipVal = offersPerPage * (page - 1) < 0 ? 0 : offersPerPage * (page - 1)  
-
-        /////-----
-        const {phoneNumber} = queryParams
-
-        if(phoneNumber) { 
-            PhoneNumbers.find({phoneNumber}).then((numbers)=>{
-        
-                let promiseOffers = []
-                numbers.forEach((e)=>{
-                    promiseOffers.push(
-                        Offer.findById(e.offerId)
-                        .populate("propertyTypeId")
-                        .populate("constructionTypeId")
-                        .populate("state")
-                        .populate("neighborhoodId"))
-                })
-    
-                Promise.all(promiseOffers).then((offers)=>{
-
-                    countOffers= offers.length
-                    let lastPageNbr = countOffers / offersPerPage
-                    res.send({
-                        offers,
-                        page,
-                        countOffers,
-                        offersPerPage,
-                        lastPageNbr
-                    })
-
-                })
-            })
-            return
-        }
-        
-        /////-----
-        
-
-        if(searchObj) {
-                Offer.find(searchObj)
-                .skip(skipVal)
-                .limit(offersPerPage)
-                .populate("propertyTypeId")
-                .populate("constructionTypeId")
-                .populate("state")
-                .populate("neighborhoodId")
-                .then((offers)=>{
-
-                    Offer.count(searchObj, function(err, countOffers) {
-                        countOffers = Number(countOffers)
-                        let lastPageNbr = countOffers / offersPerPage
-                        if(!Number.isInteger(lastPageNbr)){
-                            lastPageNbr = Math.floor(lastPageNbr) + 1
-                        }
-                        res.send({
-                            offers,
-                            page,
-                            countOffers,
-                            offersPerPage,
-                            lastPageNbr
-                        })
-                });
-            })
-        return
-        }
-
-        Offer.find()
-            .skip(skipVal)
-            .limit(offersPerPage)
-            .populate("propertyTypeId")
-            .populate("constructionTypeId")
-            .populate("state")
-            .populate("neighborhoodId")
-            .then((offers)=>{
-
-                Offer.count({}, function(err, countOffers) {
-                    countOffers = Number(countOffers)
-                    let lastPageNbr = countOffers / offersPerPage
-                    if(!Number.isInteger(lastPageNbr)){
-                        lastPageNbr = Math.floor(lastPageNbr) + 1
-                    }
-                    res.send({
-                        offers,
-                        page,
-                        countOffers,
-                        offersPerPage,
-                        lastPageNbr
-                    })
-               });
-
-                // res.send({offers: offers,page:page})
-        })
-        
-    })
-    app.get('/api/offer/:id', (req,res)=>{
-
-        Offer.findById(req.params.id, (err,offer)=>{
-            if(err){
-                console.log('Error')
-                res.send(err)
-                return
-            }
-            res.send(offer)
-        })
-    })
-
-    app.put('/api/offer/:id', (req, res) => {
-        console.log('/api/offer (UPDATE): =============')
-        console.log(req.body)
-        // console.log(req.params)
-        const newOffer = {
-            area:         req.body.area,
-            description:  req.body.description,
-            phoneNumber:  req.body.phoneNumber,
-            // phoneNumbers: [{type: String, type: String, lowercase: true, trim: true}],
-            price:       Number(req.body.price),
-            address:     req.body.address,
-            info:        req.body.info,
-            propertyOwnerName: req.body.propertyOwnerName,
-            floor:       Number(req.body.floor),
-            constructionTypeId: req.body.constructionType,
-            propertyTypeId:     req.body.propertyType,
-            state:              req.body.state,
-            neighborhoodId:     req.body.neighborhood,
-            lastCall:           new Date(req.body.lastCall),
-            nextCall:           new Date(req.body.nextCall)   
-        }
-
-        Offer.update(
-            {_id:req.params.id}, 
-            newOffer, 
-            {multi: false}, 
-            function(err, newObj){
-                if(err){
-                    console.log('ERROR')
-                    console.log(err)
-                    res.send(err)
-                    return
-                }
-
-                console.log('updated obj')
-                console.log(newObj)
-
-                res.send(newObj)
-        })
     })
 }
--- services/offers.js
+++ services/offers.js
@@ -5,10 +5,8 @@ const ConstructionType = mongoose.model('constructionTypes')
 const PropertyType = mongoose.model('propertyTypes')
 const States = mongoose.model('states')
 const Neighborhood = mongoose.model('neighborhoods')
+const PhoneNumbers = mongoose.model('phoneNumbers')
 
-function getAllOffers(params) {
-    return User.find({})
-}
 
 function addNewDetails(params){
 
@@ -37,13 +35,126 @@ function addNewDetails(params){
 
 }
 
-function getOptionDetails(){
-    let returnObj = {}
-    return ConstructionType
+function getOffer(id) {
+
+    function getRes(resolve, reject){
+        Offer.findById(id, (err,offer)=>{
+            if(err){
+                console.log('===Error===')
+                reject(err)
+                return
+            }
+            return resolve(offer)
+        })
+    }
+    return new Promise(getRes)
+}
+
+function createFilters(queryParams) {
+    let searchObj = null
+    if(queryParams) {
+        searchObj = {}
+        if(queryParams.constructionType) { 
+            searchObj.constructionTypeId = queryParams.constructionType 
+        }
+        if(queryParams.propertyType){
+            searchObj.propertyTypeId = queryParams.propertyType 
+        }
+        if(queryParams.state){
+            searchObj.state = queryParams.state 
+        }
+        if(queryParams.neighborhood){
+            searchObj.neighborhoodId = queryParams.neighborhood 
+        }
+        if(queryParams.nextCall && queryParams.nextCall !== 'Invalid date'){
+            searchObj.nextCall = {"$gte": new Date(queryParams.nextCall)}
+        }
+    }
+    return searchObj
+}
+
+function findOffersByPhone(phoneNumber, page, offersPerPage){
+
+    function getRes(resolve, reject){
+        PhoneNumbers.find({phoneNumber}).then((numbers) => {
+
+            let promiseOffers = []
+            numbers.forEach((e)=>{
+                promiseOffers.push(
+                    Offer.findById(e.offerId)
+                    .populate("propertyTypeId")
+                    .populate("constructionTypeId")
+                    .populate("state")
+                    .populate("neighborhoodId"))
+            })
+    
+            Promise.all(promiseOffers).then((offers)=>{
+    
+                let countOffers= offers.length
+                const lastPageNbr = calcLastPageNbr(countOffers)
+                resolve({
+                    offers,
+                    page,
+                    countOffers,
+                    offersPerPage,
+                    lastPageNbr
+                })
+            })
+        })    
+    }
+
+    return new Promise(getRes)
+}
+
+function calculatePaginationDetails(page, offersPerPage){
+    page =  Number(page)
+    let skipVal = offersPerPage * (page - 1) < 0 ? 0 : offersPerPage * (page - 1)  
+    return skipVal
+}
+
+function calcLastPageNbr(countOffers, offersPerPage){
+    countOffers = Number(countOffers)
+    let lastPageNbr = countOffers / offersPerPage
+    if(!Number.isInteger(lastPageNbr)){
+        lastPageNbr = Math.floor(lastPageNbr) + 1
+    }
+    return lastPageNbr
+}
+
+function getAllOffers(queryParams, page, offersPerPage){
+    console.log('getAllOffers')
+    const filters = createFilters(queryParams)
+    const skipVal = calculatePaginationDetails(page, offersPerPage)
+
+    function getRes(resolve, reject){
+        
+        Offer.find(filters)
+        .skip(skipVal)  
+        .limit(offersPerPage)
+        .populate("propertyTypeId")
+        .populate("constructionTypeId")
+        .populate("state")
+        .populate("neighborhoodId")
+        .then((offers)=>{
+
+            Offer.count(filters, function(err, countOffers) {
+                const lastPageNbr = calcLastPageNbr(countOffers)
+                resolve({
+                    offers,
+                    page,
+                    countOffers,
+                    offersPerPage,
+                    lastPageNbr
+                })
+            })
+        })
+    }
+    return new Promise(getRes)
 }
 
 module.exports = {
-    getAllOffers: getAllOffers,
-    addNewDetails: addNewDetails,
-    getOptionDetails: getOptionDetails
+    getOffer,
+    getAllOffers,
+    addNewDetails,
+    findOffersByPhone
 }
